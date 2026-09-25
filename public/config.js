c086b8215cce22e377100683d689583a8005f191
Orotsuki <138530394+Orotsuki@users.noreply.github.com>
2026-09-25 13:59:02 -0300

    Preparar public/config.js

---
+++ public/config.js
@@ -0,0 +1,6 @@
+window.NF_SCANNER_CONFIG = {
+  // Preencha estes dois campos com os dados públicos do seu projeto Supabase.
+  // Use a chave "publishable" (sb_publishable_...), nunca a secret/service_role.
+  supabaseUrl: '',
+  supabasePublishableKey: '',
+}
